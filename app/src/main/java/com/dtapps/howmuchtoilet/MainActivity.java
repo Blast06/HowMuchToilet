3223918ac253061f165490fc39d77ddc44166098
Blast06 <lacalculadora06@hotmail.com>
2020-03-25 09:38:38 -0400

    Initial commit

---
+++ app/src/main/java/com/dtapps/howmuchtoilet/MainActivity.java
@@ -0,0 +1,163 @@
+package com.dtapps.howmuchtoilet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    private SeekBar rollsBar;
+    private SeekBar toiletBar;
+    private SeekBar wipesBar;
+    private SeekBar peopleBar;
+    private SeekBar sheetsBar;
+    private SeekBar sheetsOnRollsBar;
+    private SeekBar daysOfQuarantineBar;
+
+
+
+    private TextView title1;
+    private TextView title2;
+    private TextView title3;
+    private TextView rollsTxt;
+    private TextView toiletTxt;
+    private TextView wipesTxt;
+    private TextView peopleTxt;
+    private TextView sheetsTxt;
+    private TextView sheetsOnRollsTxt;
+    private TextView daysOfQuarantineTxt;
+    public float days = 14, percent = 0, rolls = 10, toilet = 3, people = 1, sheets = 2, wipes = 5,  quar =1,
+    rollRange = 1, sheetsOnRolls = 160, daysOfQuarantine = 14, sheetsPerDay, rollsPerDay, totalSheets;
+    private boolean show = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calcular(daysOfQuarantine, rolls, sheetsOnRolls,toilet,people,sheets,wipes);
+
+
+        title1 = findViewById(R.id.title1);
+        title2 = findViewById(R.id.title2);
+        title3 = findViewById(R.id.title3);
+        title2.setText(" You will last: " + days);
+        title3.setText(" Or: " + percent + " of your quarantine");
+
+
+        rollsTxt = findViewById(R.id.rollsTxt);
+        toiletTxt = findViewById(R.id.toiletTxt);
+        wipesTxt = findViewById(R.id.wipesTxt);
+        peopleTxt = findViewById(R.id.peopleTxt);
+        sheetsTxt = findViewById(R.id.sheetsTxt);
+        sheetsOnRollsTxt = findViewById(R.id.sheetsOnRollsTxt);
+        daysOfQuarantineTxt = findViewById(R.id.daysOfQuarantineTxt);
+
+        rollsBar = findViewById(R.id.rollsBar);
+        toiletBar = findViewById(R.id.toiletBar);
+        wipesBar = findViewById(R.id.wipesBar);
+        peopleBar = findViewById(R.id.peoplesBar);
+        sheetsBar = findViewById(R.id.sheetsBar);
+        daysOfQuarantineBar = findViewById(R.id.daysOfQuarantineBar);
+        sheetsOnRollsBar = findViewById(R.id.sheetsOnRollsBar);
+
+
+        rollsBar.setOnSeekBarChangeListener(this);
+        toiletBar.setOnSeekBarChangeListener(this);
+        wipesBar.setOnSeekBarChangeListener(this);
+        peopleBar.setOnSeekBarChangeListener(this);
+        sheetsBar.setOnSeekBarChangeListener(this);
+        daysOfQuarantineBar.setOnSeekBarChangeListener(this);
+        sheetsOnRollsBar.setOnSeekBarChangeListener(this);
+
+        rollsBar.setProgress((int)rolls);
+
+
+        toiletBar.setProgress((int)toilet);
+        wipesBar.setProgress((int)wipes);
+
+        peopleBar.setProgress((int)people);
+
+        sheetsBar.setProgress((int)sheets);
+
+        daysOfQuarantineBar.setProgress((int)daysOfQuarantine);
+
+        sheetsOnRollsBar.setProgress((int)sheetsOnRolls);
+
+
+        rollsTxt.setText("Rolls you have: " + rollsBar.getProgress());
+        toiletTxt.setText("Toilet visits: " + toiletBar.getProgress());
+        wipesTxt.setText("Average wipes per trip: " + wipesBar.getProgress());
+        peopleTxt.setText("People in household: " + peopleBar.getProgress());
+        sheetsTxt.setText("Sheets per wipe: " + sheetsBar.getProgress());
+        sheetsOnRollsTxt.setText("Sheets on roll: " + daysOfQuarantineBar.getProgress());
+        daysOfQuarantineTxt.setText("Days of quarantine: " + sheetsOnRollsBar.getProgress());
+
+
+    }
+
+    private void calcular(float daysOfQuarantine, float rolls, float sheetsOnRolls, float toilet, float people, float sheets, float wipes  ) {
+
+        sheetsPerDay = sheets * wipes * (toilet * people);
+        rollsPerDay = sheetsOnRolls / sheetsPerDay;
+        totalSheets = rolls * sheetsOnRolls;
+        days = Math.round(totalSheets / sheetsPerDay);
+        percent = Math.round((days / daysOfQuarantine) * 100);
+        title2.setText(" You will last: " + days);
+        title3.setText(" Or: " + percent + " of your quarantine");
+
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+        switch (seekBar.getId()){
+            case R.id.rollsBar:
+                rollsTxt.setText("Rolls you have: " + i);
+                calcular(daysOfQuarantine, rolls, sheetsOnRolls,toilet,people,sheets,wipes);
+                break;
+            case R.id.toiletBar:
+                toiletTxt.setText("Toilet visits: " + i);
+                calcular(daysOfQuarantine, rolls, sheetsOnRolls,toilet,people,sheets,wipes);
+                break;
+            case R.id.wipesBar:
+                wipesTxt.setText("Average wipes per trip: " + i);
+                calcular(daysOfQuarantine, rolls, sheetsOnRolls,toilet,people,sheets,wipes);
+                break;
+            case R.id.peoplesBar:
+                peopleTxt.setText("People in household: " + i);
+                calcular(daysOfQuarantine, rolls, sheetsOnRolls,toilet,people,sheets,wipes);
+                break;
+            case R.id.sheetsBar:
+                sheetsTxt.setText("Sheets per wipe: " + i);
+                calcular(daysOfQuarantine, rolls, sheetsOnRolls,toilet,people,sheets,wipes);
+                break;
+            case R.id.daysOfQuarantineBar:
+                daysOfQuarantineTxt.setText("Days of quarantine: " + i);
+                calcular(daysOfQuarantine, rolls, sheetsOnRolls,toilet,people,sheets,wipes);
+                break;
+
+            case R.id.sheetsOnRollsBar:
+                sheetsOnRollsTxt.setText("Sheets on roll: " + i);
+                calcular(daysOfQuarantine, rolls, sheetsOnRolls,toilet,people,sheets,wipes);
+                break;
+
+
+        }
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
